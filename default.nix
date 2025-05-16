07c41530a3b920fb72f34dfa2f3cb8c4e7a15f09
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-05-17 00:35:03 +0800

    chore: refactor variables for replacement

--- default.nix
+++ default.nix
@@ -4,6 +4,11 @@
   dream2nix,
   ...
 }:
+let
+  name = "prisma-language-server";
+  version = "6.8.1";
+  sha256 = "sha256-mdS9x4cEWj1gH0zr+milt59n7Wo9NY39MFXtGNkPBEs=";
+in
 {
   imports = [
     dream2nix.modules.dream2nix.nodejs-package-json-v3
@@ -25,12 +30,12 @@
     runBuild = false;
   };
 
-  name = lib.mkForce "prisma-language-server";
-  version = lib.mkForce "6.8.1";
+  name = lib.mkForce name;
+  version = lib.mkForce version;
 
   mkDerivation = {
     src =
-      config.deps.nixpkgs.runCommand "prisma-language-server-src"
+      config.deps.nixpkgs.runCommand "${name}-src"
         {
           inherit (config.deps.nixpkgs)
             bash
@@ -39,8 +44,8 @@
             gzip
             ;
           srcTarball = builtins.fetchurl {
-            url = "https://registry.npmjs.org/@prisma/language-server/-/language-server-6.8.1.tgz";
-            sha256 = "sha256:0jq41zciivam63yqsd9xdbnng7xplmlgmssc3xh3snh4hz3vvm4r";
+            url = "https://registry.npmjs.org/@prisma/language-server/-/language-server-${version}.tgz";
+            sha256 = sha256;
           };
         }
         ''
@@ -50,7 +55,7 @@
         '';
     # dontBuild = true;
     installPhase = ''
-      chmod +x $out/bin/prisma-language-server
+      chmod +x $out/bin/${name}
     '';
 
     postInstall = ''
